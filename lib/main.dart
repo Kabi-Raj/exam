38e0194f411bce9cf95740be9d564e354ef7a5be
Kabiraj Chauhan <kabiraj@Kabirajs-MacBook-Pro.local>
2019-12-08 12:44:59 +0545

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:exam/model/scopedModel.dart';
+import 'package:exam/screens/documents.dart';
+import 'package:exam/screens/theory_round.dart';
+import 'package:flutter/material.dart';
+import 'package:scoped_model/scoped_model.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final MainScopedModel model = MainScopedModel();
+    return ScopedModel(
+      model: model,
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        /* theme: ThemeData(
+            primaryColor: Colors.blue[300], 
+            accentColor: Colors.blue[200]), */
+        routes: {
+          '/theory': (BuildContext context) => Theory(model),
+          '/': (BuildContext context) => Documents(),
+        },
+      ),
+    );
+  }
+}
